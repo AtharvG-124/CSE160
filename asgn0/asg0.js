51dde582c93710dac9aaff9cb5413bcb75c854f4
Atharv Gupta <atharv.gupta124@gmail.com>
2026-04-05 15:28:54 -0700

    part3 done

--- asgn0/asg0.js
+++ asgn0/asg0.js
@@ -4,13 +4,23 @@ function main() {
         console.log('Failed to retrieve the <canvas> element');
         return;
     }
+    var ctx = canvas.getContext('2d');
 
+    ctx.fillStyle = 'rgba(0, 0, 0, 1.0)';
+    ctx.fillRect(0, 0, 400, 400);
+}
+
+function handleDrawEvent() {
+    var canvas = document.getElementById('example');
     var ctx = canvas.getContext('2d');
 
     ctx.fillStyle = 'rgba(0, 0, 0, 1.0)';
     ctx.fillRect(0, 0, 400, 400);
 
-    var v1 = new Vector3([2.25, 2.25, 0]);
+    var x = document.getElementById('v1X').value;
+    var y = document.getElementById('v1Y').value;
+
+    var v1 = new Vector3([parseFloat(x), parseFloat(y), 0]);
 
     drawVector(v1, "red");
 }
