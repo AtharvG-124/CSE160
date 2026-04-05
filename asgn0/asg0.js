28151e642fc2ae05186e2a0a6e64e5989c59f5e6
Atharv Gupta <atharv.gupta124@gmail.com>
2026-04-05 15:27:13 -0700

    part2 done

--- asgn0/asg0.js
+++ asgn0/asg0.js
@@ -1,18 +1,34 @@
-// DrawRectangle.js
 function main() {
-    // Retrieve <canvas> element <- (1)
     var canvas = document.getElementById('example');
     if (!canvas) {
         console.log('Failed to retrieve the <canvas> element');
         return;
-
     }
 
-    // Get the rendering context for 2DCG <- (2)
     var ctx = canvas.getContext('2d');
 
-    // Draw a blue rectangle <- (3)
-    ctx.fillStyle = 'rgba(0, 0, 255, 1.0)'; // Set a blue color
-    ctx.fillRect(120, 10, 150, 150); // Fill a rectangle with the color
+    ctx.fillStyle = 'rgba(0, 0, 0, 1.0)';
+    ctx.fillRect(0, 0, 400, 400);
 
+    var v1 = new Vector3([2.25, 2.25, 0]);
+
+    drawVector(v1, "red");
+}
+
+function drawVector(v, color) {
+    var canvas = document.getElementById('example');
+    var ctx = canvas.getContext('2d');
+
+    ctx.strokeStyle = color;
+
+    var x = v.elements[0];
+    var y = v.elements[1];
+
+    var dx = x * 20;
+    var dy = y * 20;
+
+    ctx.beginPath();
+    ctx.moveTo(200, 200);
+    ctx.lineTo(200 + dx, 200 - dy);
+    ctx.stroke();
 }
